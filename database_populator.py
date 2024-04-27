05be3f2ff9b67df3b591997a8bc84351920cd9cd
Ayubabe <chaudhari.ayush2001@gmail.com>
2024-04-27 13:50:12 -0600

    College stats
    Found a DB that contains college stats from 2009 - 2022. Currently cleaning it up for us. Still not sure about getting stats for 2023 draft or international draft picks

--- database_populator.py
+++ database_populator.py
@@ -7,15 +7,17 @@ NBA_team_ids = [1610612759, 1610612765, 1610612753, 1610612754, 1610612749, 1610
  1610612738, 1610612739, 1610612748, 1610612764, 1610612746, 1610612758,
  1610612741, 1610612757, 1610612760, 1610612761, 1610612737, 1610612752,
  1610612745, 1610612744, 1610612747, 1610612743, 1610612762]
-
+Colleges = None
 
 # Used to get Historical Draft data from nba_api
 def GetDraftData():
+    global Colleges
     drafts = nba.DraftHistory()
     draft_df = pd.DataFrame(drafts.get_data_frames()[0])
     draft_df['TEAM'] = draft_df['TEAM_CITY'] + ' ' + draft_df['TEAM_NAME']
     draft_df_frp = draft_df[(draft_df['OVERALL_PICK'] <= 30) & (draft_df['OVERALL_PICK'] > 0)]
     return draft_df_frp[['PERSON_ID','PLAYER_NAME','SEASON','OVERALL_PICK','TEAM_ID','TEAM']]
+
 
 
 # Used to get Team stats per year (which will be useful in deciding what the team needs are)
@@ -33,16 +35,19 @@ def GetTeamStats(NBA_team_ids):
     return teams_df
 
 
+college_stats = pd.read_csv('CollegeBasketballPlayers2022.csv')
+college_stats = pd.concat([college_stats,pd.read_csv('CollegeBasketballPlayers2009-2021.csv')],axis=0)
 
 first_round_picks = GetDraftData()
-teams_yearly_stats = GetTeamStats(NBA_team_ids)
+#teams_yearly_stats = GetTeamStats(NBA_team_ids)
 
-teams_draft_pick = first_round_picks.merge(teams_yearly_stats,on=['TEAM_ID','SEASON']).drop(columns=['TEAM_CITY','TEAM_NAME'])
+#test = first_round_picks.merge(college_stats, left_on='PLAYER_NAME', right_on='player_name')
+print(college_stats.columns)
+print(college_stats.groupby('player_name')[['usg','Ortg','adrtg','pts','dreb','oreb','dreb','treb','ast','stl','blk','ast/tov']].mean())
 
 
-print(teams_draft_pick)
 
-
+#teams_draft_pick_stats = teams_draft_pick.merge(college_stats,on=['TEAM_ID','SEASON']).drop(columns=['TEAM_CITY','TEAM_NAME'])
 
 
 
