37e5b8c380b347946ada130ef90fed68b71b39fd
Ayubabe <chaudhari.ayush2001@gmail.com>
2024-05-02 20:38:37 -0600

    Changed scope
    Changed project to now see which draft pick deserves ROTY by seeing how much they impacted the teams performance

--- database_populator.py
+++ database_populator.py
@@ -31,25 +31,42 @@ def GetTeamStats(NBA_team_ids):
         teams_df = pd.concat([teams_df,team_df],axis=0)
 
     teams_df['SEASON'] = teams_df['YEAR'].map(lambda x: x.split('-')[0][:2] + x.split('-')[1]) # This is used to decide the Draft Season and correlate it to the stats
-    
+    teams_df['DRAFTED_SEASON'] = teams_df['YEAR'].map(lambda x: str(int(x.split('-')[0][:2] + x.split('-')[1]) - 1))
     return teams_df
 
 
-college_stats = pd.read_csv('CollegeBasketballPlayers2022.csv')
-college_stats = pd.concat([college_stats,pd.read_csv('CollegeBasketballPlayers2009-2021.csv')],axis=0)
+#Player college stats
+# college_stats = pd.read_csv('CollegeBasketballPlayers2022.csv')
+# college_stats = pd.concat([college_stats,pd.read_csv('CollegeBasketballPlayers2009-2021.csv')],axis=0)
+# college_stats['player_name'] = college_stats['player_name'].map(lambda x: x.upper())
+# college_stats.rename(columns={"Unnamed: 64":"Position"},inplace=True)
+# mean_college_stats = college_stats.groupby('player_name')[['usg','Ortg','adrtg','pts','dreb','oreb','dreb','treb','ast','stl','blk','ast/tov']].mean().reset_index()
+# mean_college_stats = mean_college_stats.merge(college_stats[['player_name','Position']],on='player_name').drop_duplicates(['player_name'])
 
-college_stats['player_name'] = college_stats['player_name'].map(lambda x: x.upper())
-college_stats.rename(columns={"Unnamed: 64":"Position"},inplace=True)
+
+#Draft Picts
 first_round_picks = GetDraftData()
+first_round_picks['PLAYER_NAME'] = first_round_picks['PLAYER_NAME'].map(lambda x: x.upper())
+
+
+#Team Stats in previous year
 #teams_yearly_stats = GetTeamStats(NBA_team_ids)
+#print(teams_yearly_stats[teams_yearly_stats['DRAFTED_SEASON'] == '2023'])
+#teams_yearly_stats = teams_yearly_stats[['TEAM_ID','SEASON','DRAFTED_SEASON','WINS','LOSSES','WIN_PCT','FGM','FGA','FG_PCT','FG3M','FG3A','FG3_PCT','FT_PCT','OREB','DREB','REB','AST','PF','STL','TOV','BLK','PTS','PTS_RANK']]
 
-#test = first_round_picks.merge(college_stats, left_on='PLAYER_NAME', right_on='player_name')
+test = nba.PlayerCareerStats(player_id=77869)
+test_db = pd.DataFrame(test.get_data_frames()[0])
 
-mean_college_stats = college_stats.groupby('player_name')[['usg','Ortg','adrtg','pts','dreb','oreb','dreb','treb','ast','stl','blk','ast/tov']].mean().reset_index()
-mean_college_stats = mean_college_stats.merge(college_stats[['player_name','Position']],on='player_name').drop_duplicates(['player_name'])
+print(test_db.columns)
+#frp_college_stats = first_round_picks.merge(mean_college_stats, left_on='PLAYER_NAME', right_on='player_name')
 
-#teams_draft_pick_stats = teams_draft_pick.merge(college_stats,on=['TEAM_ID','SEASON']).drop(columns=['TEAM_CITY','TEAM_NAME'])
 
+#teams_draft_pick_stats = first_round_picks.merge(teams_yearly_stats,on=['SEASON','TEAM_ID'])
+
+
+#print(frp_college_stats[['PLAYER_NAME','pts']])
+
+#print(teams_draft_pick_stats[teams_draft_pick_stats['STL'] > 0])
 
 
 
